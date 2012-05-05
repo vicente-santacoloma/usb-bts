70395242e396ce49bac12b7ee5ab2d3a69439ac4
Bishma Stornelli <tamerdark@gmail.com>
2012-05-05 10:09:06 -0400

    Modelo de apps users y social creado. Agregado archivo .gitignore para ignorar archivos .pyc

--- social/models.py
+++ social/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Message(models.Model):
+    content = models.TextField()
+    date_sent = models.DateTimeField(auto_now_add=True)
+    sender = models.ForeignKey('users.user', related_name='messages_sent')
+    receiver = models.ForeignKey('users.user', related_name='messages_received')
+
+    def __unicode__(self):
+        return "From " + self.sender.get_username() + " to " + self.receiver.get_username() + " on " + self.date_sent + ":\n" + self.content
--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=16, unique=True)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=128)
+    USERS_CHOICES = (
+        ('A', 'Administrator'),
+        ('G', 'Gate Keeper'),
+        ('U', 'User'),
+    )
+    role = models.CharField(max_length=1, choices=USERS_CHOICES)
+
+    def __unicode__(self):
+        return "Username: " + self.username + "\nEmail: " + self.email + "\nRole: " + self.get_role_display()
